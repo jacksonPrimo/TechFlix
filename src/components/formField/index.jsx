349260d5c123c1183c77724be2986ea80eb00386
JACKSON PRIMO <jackson.primo12@gmail.com>
2020-07-31 22:15:56 -0300

    estilização e organização do formulario adicionados

--- src/components/formField/index.jsx
+++ src/components/formField/index.jsx
@@ -1,12 +1,102 @@
-import React from 'react'
+import React from 'react';
+import propTypes from 'prop-types';
+import styled, { css } from 'styled-components';
 
-export default function FormField({Tag, labelText, type, value, nome, onChange}){
-    return(
-        <div>
-            <label>
-                {labelText}
-                <Tag value={value} type={type} onChange={onChange}/>
-            </label>
-        </div>
-    )
+const FormFieldWrapper = styled.div`
+  position: relative;
+  textarea {
+    min-height: 150px;
+  }
+  input[type="color"] {
+    padding-left: 56px;
+  }
+`;
+
+const Label = styled.label``;
+
+Label.Text = styled.span`
+  color: #E5E5E5;
+  height: 57px;
+  position: absolute; 
+  top: 0;
+  left: 16px;
+  
+  display: flex;
+  align-items: center;
+  
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  
+  transition: .1s ease-in-out;
+`;
+
+const Input = styled.input`
+  background: #53585D;
+  color: #F5F5F5;
+  display: block;
+  width: 100%;
+  height: 57px;
+  font-size: 18px;
+  
+  outline: 0;
+  border: 0;
+  border-top: 4px solid transparent;
+  border-bottom: 4px solid #53585D;
+  
+  padding: 16px 16px;
+  margin-bottom: 45px;
+  
+  resize: none;
+  border-radius: 4px;
+  transition: border-color .3s;
+  
+  &:focus {
+    border-bottom-color: var(--primary);
+  }
+  &:focus:not([type='color']) + ${Label.Text} {
+    transform: scale(.6) translateY(-10px);
+  }
+  ${({ value }) => {
+    const hasValue = value.length > 0;
+    return hasValue && css`
+        &:not([type='color']) + ${Label.Text} {
+          transform: scale(.6) translateY(-10px);
+        }
+      `;
+  }
 }
+`;
+
+export default function FormField({
+  Tag, labelText, type, value, nome, onChange,
+}) {
+  const fieldId = `id_${nome}`;
+  return (
+    <FormFieldWrapper>
+      <Label
+        htmlFor={fieldId}
+      >
+        <Input as={Tag} value={value} type={type} onChange={onChange} />
+        <Label.Text>
+          {labelText}
+        </Label.Text>
+      </Label>
+    </FormFieldWrapper>
+  );
+}
+
+FormField.defaultProps = {
+  type: 'text',
+  value: '',
+  onChange: () => {},
+};
+FormField.propTypes = {
+  Tag: propTypes.string.isRequired,
+  labelText: propTypes.string.isRequired,
+  type: propTypes.string,
+  value: propTypes.string,
+  nome: propTypes.string.isRequired,
+  onChange: propTypes.func,
+};
--- src/pages/cadastro/categoria/index.jsx
+++ src/pages/cadastro/categoria/index.jsx
@@ -1,67 +1,84 @@
-import React, {useState} from 'react';
-import {Link} from 'react-router-dom'
-import PageDefault from '../../../components/pageDefault'
-import FormField from '../../../components/formField'
+/* eslint-disable linebreak-style */
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/pageDefault';
+import FormField from '../../../components/formField';
+import Button from '../../../components/button';
 
-export default function CadastroCategoria(){
-  const [categorias, setCategorias] = useState([])    
-  
+export default function CadastroCategoria() {
+  const [categorias, setCategorias] = useState([]);
   const valoresIniciais = {
     nome: '',
     descricao: '',
-    cor: '#000'
-  }
+    cor: '#000',
+  };
   const [Values, setValues] = useState(valoresIniciais);
-  
-  function setValue(chave, valor){
+  function setValue(chave, valor) {
     setValues({
       ...Values,
-      [chave]: valor
-    })
+      [chave]: valor,
+    });
   }
 
-  return(
-      <PageDefault>
-        <h1>Cadastrar Categoria: {Values.nome}</h1>
-        <form onSubmit={e=>{
-          e.preventDefault()
-          setCategorias([...categorias, Values])
-          setValues(valoresIniciais)
-        }}>
-          <FormField
-            Tag = "input"
-            labelText="Nome da Categoria: "
-            type="text"
-            value={Values.nome}
-            onChange={e=>{setValue('nome',e.target.value)}}
-          />
-          <FormField
-            Tag = "textarea"
-            labelText="Descrição: "
-            type="text"
-            value={Values.descricao}
-            onChange={e=>{setValue('descricao',e.target.value)}}
-          />
-          <FormField
-            Tag = "input"
-            labelText="cor"
-            type="color"
-            value={Values.cor}
-            onChange={e=>{setValue('cor',e.target.value)}}
-          />
-          <button>Cadastrar</button>
-        </form>
-        <ul>
-          {categorias.map((cat, indice)=>{
-            return(
-              <li key={indice}>{cat.nome}</li>  
-            )
-          })}
-        </ul>
+  useEffect(() => {
+    const URL = 'http://localhost:3001/categorias';
+    fetch(URL)
+      .then(async (resp) => {
+        const response = await resp.json();
+        //console.log(response);
+        setCategorias([
+          ...response,
+        ]);
+      });
+  }, []);
 
-        <Link to="/">
-          Ir para Home
-        </Link>
-      </PageDefault>
-    )
-  }
+  return (
+    <PageDefault>
+      <h1>
+        Cadastrar Categoria:
+        {Values.nome}
+      </h1>
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        setCategorias([...categorias, Values]);
+        setValues(valoresIniciais);
+      }}
+      >
+        <FormField
+          Tag="input"
+          labelText="Nome da Categoria: "
+          type="text"
+          value={Values.nome}
+          onChange={(e) => { setValue('nome', e.target.value); }}
+        />
+        <FormField
+          Tag="textarea"
+          labelText="Descrição: "
+          type="text"
+          value={Values.descricao}
+          onChange={(e) => { setValue('descricao', e.target.value); }}
+        />
+        <FormField
+          Tag="input"
+          labelText="cor"
+          type="color"
+          value={Values.cor}
+          onChange={(e) => { setValue('cor', e.target.value); }}
+        />
+        <Button>Cadastrar</Button>
+      </form>
+      {categorias.lenght === 0 && (
+        <div>loading</div>
+      )}
+      <ul>
+        {categorias.map((cat, indice) => (
+          <li key={cat.nome}>{cat.nome}</li>
+        ))}
+      </ul>
+
+      <Link to="/">
+        Ir para Home
+      </Link>
+    </PageDefault>
+  );
+}
